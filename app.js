6bbf070b3ddba711007f32530900040851f51b66
Shemuel Chukwukadibia <shemmy80@gmail.com>
2024-04-16 22:39:29 +0100

    Add Javascript

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// alert('Hello world');
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+-=[]{}\\|;\':",./<>?';
+
+const lengthEl = document.getElementById('length');
+const lowercaseEl = document.getElementById('lowercase');
+const uppercaseEl = document.getElementById('uppercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateBtn = document.getElementById('generate');
+const passwordEl = document.getElementById('password');
+
+generateBtn.addEventListener('click', function () {
+  const length = lengthEl.value;
+  let characters = '';
+  let password = '';
+
+  if (lowercaseEl.checked) {
+    characters = lowercaseLetters;
+  }
+
+  if (uppercaseEl.checked) {
+    characters += uppercaseLetters;
+  }
+
+  if (numbersEl.checked) {
+    characters += numbers;
+  }
+
+  if (symbolsEl.checked) {
+    characters += symbols;
+  }
+
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
+  passwordEl.value = password;
+
+  if (
+    !lowercaseEl.checked &&
+    !uppercaseEl.checked &&
+    !numbersEl.checked &&
+    !symbolsEl.checked
+  ) {
+    alert('No box checked!');
+  }
+});
